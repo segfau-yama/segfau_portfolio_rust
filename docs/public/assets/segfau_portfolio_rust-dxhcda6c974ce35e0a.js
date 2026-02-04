720a09f5c06d561777bf111754adeacdcdd5d008
segfau-yama <a2211528@sendai-nct.jp>
2026-02-04 22:43:29 +0900

    v0.0.1

--- docs/public/assets/segfau_portfolio_rust-dxhcda6c974ce35e0a.js
+++ docs/public/assets/segfau_portfolio_rust-dxhcda6c974ce35e0a.js
@@ -3487,7 +3487,7 @@ async function __wbg_init(module_or_path) {
 globalThis.__wasm_split_main_initSync = initSync;
 __wbg_init({
   module_or_path:
-    "/segfau_portfolio_rust/docs/assets/segfau_portfolio_rust_bg-dxh8fd97b7ce420bcb3.wasm",
+    "/segfau_portfolio_rust/assets/segfau_portfolio_rust_bg-dxh8fd97b7ce420bcb3.wasm",
 }).then((wasm) => {
   globalThis.__dx_mainWasm = wasm;
   globalThis.__dx_mainInit = __wbg_init;
